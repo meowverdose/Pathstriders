868f9dd23339e69ee5df7b87290fa77ce0da0f95
kobechu <aznmantran@gmail.com>
2023-06-07 20:28:23 -0700

    Added /whatis

---
+++ src/main/java/cc/chocochip/seele/commands/WhatisCommand.java
@@ -0,0 +1,35 @@
+package cc.chocochip.seele.commands;
+
+import cc.chocochip.seele.Seele;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class WhatisCommand implements CommandExecutor {
+
+    private final Seele plugin;
+
+    public WhatisCommand(Seele plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String prefix, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + " **ERROR** This command can only be ran by players");
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.isOp()) {
+            player.sendMessage(ChatColor.RED + " **ACCESS DENIED** Invalid permissions");
+            return false;
+        }
+
+        player.sendMessage(player.getInventory().getItemInMainHand().toString().replaceAll(",", "\n"));
+        return true;
+    }
+}

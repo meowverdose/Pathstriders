89e3811e8b4463feb8100c32b55bf2b90baa65bd
kobechu <aznmantran@gmail.com>
2023-06-10 13:26:06 -0700

    Fixed attribute modifiers in Talents menu

--- src/main/java/org/nekoverse/seele/talents/TalentsMenu.java
+++ src/main/java/org/nekoverse/seele/talents/TalentsMenu.java
@@ -1,6 +1,8 @@
 package org.nekoverse.seele.talents;
 
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.nekoverse.seele.data.PlayerData;
@@ -12,6 +14,9 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.nekoverse.seele.utils.ItemBuilder;
+
+import javax.annotation.Nullable;
+import java.util.Map;
 
 public class TalentsMenu implements Menu {
 
@@ -33,21 +38,24 @@ public class TalentsMenu implements Menu {
                         if (cursor == null) return;                                         // Removing talent
 
                         if (!isTalentItem(cursor)) {                                        // Only talents
-                            System.out.println("cancel B"); // todo DBUG
                             event.setCancelled(true);
+                            return;
                         }
 
                         // TODO: 6/10/2023 Add specific slot logic later
+                        /*if (!isAppropriateSlot(event.getSlot(), cursor)) {
+                            event.setCancelled(true);
+                            return;
+                        }*/
                     } else {                                                                // Talent slot occupied
                         if (talent.getMaxStackSize() > 1 && talent.isSimilar(cursor)) {     // No stacking talents
-                            System.out.println("cancel A"); // todo DBUG
                             event.setCancelled(true);
+                            return;
                         }
                     }
                 }
             }
             default -> {
-                System.out.println("cancel C"); // todo DBUG
                 event.setCancelled(true);
             }
         }
@@ -55,22 +63,26 @@ public class TalentsMenu implements Menu {
 
     @Override
     public void onInventoryClose(InventoryCloseEvent event) {
-        for (int i = 0; i < this.playerData.getTalents().length; ++i) {
+        Player player = (Player) event.getPlayer();
+
+        removeAttributeModifiers(player);
+
+        for (int i = 0; i < 3; ++i) {
             int slot = (i * 3) + 10;
             ItemStack talent = event.getInventory().getItem(slot);
 
             playerData.setTalent(i, (talent == null ? null : new ItemStack(talent)));       // Must do copy constructor
-        }
 
-        Player player = Bukkit.getPlayer(playerData.getUniqueId());
+            applyAttributeModifiers(talent, player);
+        }
 
         player.sendMessage(
                 "",
                 ChatColor.GOLD + " " + ChatColor.BOLD + "Talents",
                 "",
-                ChatColor.YELLOW + "  Light Cone: " + (playerData.getTalents()[0] == null ? ChatColor.GRAY + "Empty" : playerData.getTalents()[0].getItemMeta().getDisplayName()),
-                ChatColor.YELLOW + "  Artifact: " + (playerData.getTalents()[1] == null ? ChatColor.GRAY + "Empty" : playerData.getTalents()[1].getItemMeta().getDisplayName()),
-                ChatColor.YELLOW + "  Relic: " + (playerData.getTalents()[2] == null ? ChatColor.GRAY + "Empty" : playerData.getTalents()[2].getItemMeta().getDisplayName()),
+                ChatColor.YELLOW + "  Light Cone: " + (playerData.hasLightCone() ? playerData.getLightCone().getItemMeta().getDisplayName() : ChatColor.GRAY + "Empty"),
+                ChatColor.YELLOW + "  Artifact: " + (playerData.hasArtifact() ? playerData.getArtifact().getItemMeta().getDisplayName()  : ChatColor.GRAY + "Empty"),
+                ChatColor.YELLOW + "  Relic: " + (playerData.hasRelic() ? playerData.getRelic().getItemMeta().getDisplayName()  : ChatColor.GRAY + "Empty"),
                 ""
         );
     }
@@ -80,7 +92,7 @@ public class TalentsMenu implements Menu {
         for (int i = 0; i < inventory.getSize(); ++i) {
             switch (i) {
                 case 1 -> {
-                    inventory.setItem(i, new ItemBuilder(Material.NETHER_STAR, 1)
+                    inventory.setItem(i, new ItemBuilder(Material.FILLED_MAP, 1)
                             .setName(ChatColor.GOLD + "" + ChatColor.BOLD + "Light Cone")
                             .addEnchant(Enchantment.DURABILITY, 1)
                             .addFlag(ItemFlag.HIDE_ENCHANTS)
@@ -96,14 +108,20 @@ public class TalentsMenu implements Menu {
                     );
                 }
                 case 7 -> {
-                    inventory.setItem(i, new ItemBuilder(Material.NETHER_STAR, 1)
-                            .setName(ChatColor.RED + "" + ChatColor.BOLD + "Relic")
+                    inventory.setItem(i, new ItemBuilder(Material.EMERALD, 1)
+                            .setName(ChatColor.GREEN + "" + ChatColor.BOLD + "Relic")
                             .addEnchant(Enchantment.DURABILITY, 1)
                             .addFlag(ItemFlag.HIDE_ENCHANTS)
                             .toItemStack()
                     );
                 }
-                case 10, 13, 16 -> {}
+                case 10, 13, 16 -> {
+                    int index = (i - 10) / 3;
+
+                    if (this.playerData.hasTalent(index)) {
+                        inventory.setItem(i, playerData.getTalent(index));
+                    }
+                }
                 default -> {
                     inventory.setItem(i, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE, 1)
                             .setName(" ")
@@ -113,27 +131,30 @@ public class TalentsMenu implements Menu {
             }
         }
 
-        for (int i = 0; i < this.playerData.getTalents().length; ++i) {
+        /*for (int i = 0; i < this.playerData.getTalents().length; ++i) {
             if (this.playerData.getTalents()[i] == null) return;
 
             int slot = (i * 3) + 10;                                                        // 10, 13, 16
             ItemStack talent = this.playerData.getTalents()[i];
             inventory.setItem(slot, talent);
-        }
+        }*/
     }
 
     @Override
     public Inventory getInventory() {
-        Inventory inventory = Bukkit.createInventory(this, 27, "Talents");
+        Inventory inventory = Bukkit.createInventory(this, 27, ChatColor.BLUE + "" + ChatColor.BOLD + "Talents");
         addContent(inventory);
         return inventory;
     }
 
+    /**
+     * Pretty
+     */
     public Inventory create() {
         return getInventory();
     }
 
-    private boolean isTalentItem(ItemStack item) { // TODO: 6/9/2023 Later will need to change if @item has dynamic durability
+    private boolean isTalentItem(ItemStack item) {
         if (!item.hasItemMeta()) return false;
 
         for (Items items : Items.values()) {
@@ -142,5 +163,25 @@ public class TalentsMenu implements Menu {
             }
         }
         return false;
+    }
+
+    private void applyAttributeModifiers(@Nullable ItemStack item, Player player) {
+        if (item == null) return;
+        if (!item.hasItemMeta()) return;
+        if (!item.getItemMeta().hasAttributeModifiers()) return;
+
+        for (Map.Entry<Attribute, AttributeModifier> entry : item.getItemMeta().getAttributeModifiers().entries()) {
+            player.getAttribute(entry.getKey()).addModifier(entry.getValue());
+        }
+    }
+
+    private void removeAttributeModifiers(Player player) {
+        for (Attribute attribute : Attribute.values()) {
+            if (player.getAttribute(attribute) != null) {
+                player.getAttribute(attribute).getModifiers().forEach(modifier -> {
+                    player.getAttribute(attribute).removeModifier(modifier);
+                });
+            }
+        }
     }
 }

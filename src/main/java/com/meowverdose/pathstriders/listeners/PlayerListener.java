fc0dcad89f15992b4c554306d6ee81c40b79218a
meowphetamine <meowphetamine@gmail.com>
2025-07-09 13:46:27 -0700

    Cooldown and talent disabled logic

--- src/main/java/com/meowverdose/pathstriders/listeners/PlayerListener.java
+++ src/main/java/com/meowverdose/pathstriders/listeners/PlayerListener.java
@@ -2,6 +2,7 @@ package com.meowverdose.pathstriders.listeners;
 
 import com.meowverdose.pathstriders.Pathstriders;
 import com.meowverdose.pathstriders.talents.Talent;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class PlayerListener implements Listener {
 
@@ -20,27 +22,44 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (!(event.getDamager() instanceof Player player)) return;
+        if (!(event.getDamager() instanceof Player player)) return; // Damager is player check
 
         Talent talent = plugin.getPlayerDataManager().getActiveTalent(player);
 
-        if (talent == null) return;
-        if (!(event.getEntity() instanceof LivingEntity target)) return;
+        if (talent == null) return; // Null check
+        if (!(event.getEntity() instanceof LivingEntity target)) return; // Target is player check
+        if (plugin.getPlayerDataManager().isTalentDisabled(player)) return; // Talent disabled check
 
         talent.onAttack(player, target);
         plugin.getLogger().info(player.getName() + "'s " + talent.getId() + " talent proc'd on " + target.getName());
     }
 
-    // TODO debug this
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_AIR) return;
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        Action action = event.getAction();
+
+        // Not a right-click
+        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+
+        // Ignore main-hand actions
+        if (event.getHand() != EquipmentSlot.OFF_HAND) {
+            return;
+        }
 
         Player player = event.getPlayer();
         Talent talent = plugin.getPlayerDataManager().getActiveTalent(player);
 
-        if (talent == null) return;
+        if (talent == null) return; // Null check
+        if (plugin.getPlayerDataManager().isOnCooldown(player, talent.getId())) { // Talent cooldown check
+            System.out.println("DEBUG : " + Pathstriders.getInstance().getPlayerDataManager().isOnCooldown(player, talent.getId()));
+            long remaining = Pathstriders.getInstance().getPlayerDataManager().getCooldownRemaining(player, talent.getId()) / 1000;
+
+            player.sendMessage(ChatColor.RED + "Talents: " + talent.name() + " is on cooldown! " + remaining + "s remaining.");
+            return;
+        }
+        if (plugin.getPlayerDataManager().isTalentDisabled(player)) return; // Talent disabled check
 
         talent.onRightClick(player);
     }
